3c45bdabfc704dc890f3dc2e1695e8ca3310577a
eora21 <eora21@naver.com>
2023-11-17 15:41:06 +0900

    refactor: 💡 수정 카드 일련번호와 소지자 대응표 작성

--- src/main/java/factory_method/idcard/IDCard.java
+++ src/main/java/factory_method/idcard/IDCard.java
@@ -1,13 +1,20 @@
 package factory_method.idcard;
 
 import factory_method.framework.Product;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class IDCard extends Product {
     private final String ownerName;
+    private final int idNumber;
 
-    public IDCard(String ownerName) {
+    public IDCard(String ownerName, int idNumber) {
         System.out.println(ownerName + "의 카드를 만듭니다.");
         this.ownerName = ownerName;
+        this.idNumber = idNumber;
+    }
+
+    public int getIdNumber() {
+        return idNumber;
     }
 
     @Override
--- src/main/java/factory_method/idcard/IDCardFactory.java
+++ src/main/java/factory_method/idcard/IDCardFactory.java
@@ -2,11 +2,18 @@ package factory_method.idcard;
 
 import factory_method.framework.Factory;
 import factory_method.framework.Product;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class IDCardFactory extends Factory {
+    private static final AtomicInteger ATOMIC_ID_GENERATOR = new AtomicInteger();
+    private final Map<Integer, String> idNumberAndOwnerName = new HashMap<>();
     @Override
     protected Product createProduct(String ownerName) {
-        return new IDCard(ownerName);
+        int idNumber = ATOMIC_ID_GENERATOR.incrementAndGet();
+        idNumberAndOwnerName.put(idNumber, ownerName);
+        return new IDCard(ownerName, idNumber);
     }
 
     @Override
